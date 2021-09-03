c8606db4744dff2010a355d88cfdf289ae9807f9
Dreamwalker <aristojeff@gmail.com>
2021-09-03 18:53:38 +0900

    Update boxiz_app.dart

---
+++ lib/ep901_boxiz_app/ui/boxiz_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BoxizApp extends StatelessWidget {
+  const BoxizApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
