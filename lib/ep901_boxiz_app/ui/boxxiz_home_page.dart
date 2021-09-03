b061c451792b6fb7b94233872ce2934fe39eea3a
Dreamwalker <aristojeff@gmail.com>
2021-09-03 18:53:36 +0900

    Create boxxiz_home_page.dart

---
+++ lib/ep901_boxiz_app/ui/boxxiz_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BoxizHomePage extends StatefulWidget {
+  const BoxizHomePage({Key? key}) : super(key: key);
+
+  @override
+  _BoxizHomePageState createState() => _BoxizHomePageState();
+}
+
+class _BoxizHomePageState extends State<BoxizHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
