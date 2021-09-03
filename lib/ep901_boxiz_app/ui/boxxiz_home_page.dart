91d5822543c950d18bfcc046f3a9483cf4ed42d5
Dreamwalker <aristojeff@gmail.com>
2021-09-03 18:55:51 +0900

    Update boxxiz_home_page.dart

--- lib/ep901_boxiz_app/ui/boxxiz_home_page.dart
+++ lib/ep901_boxiz_app/ui/boxxiz_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class BoxizHomePage extends StatefulWidget {
   const BoxizHomePage({Key? key}) : super(key: key);
 
@@ -12,13 +11,23 @@ class _BoxizHomePageState extends State<BoxizHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Column(
-        children: [
-          Expanded(child: Placeholder(), flex: 2,),
-          Expanded(child: Placeholder(), flex: 20,),
-          Expanded(child: Placeholder(), flex: 2,),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Placeholder(),
+              flex: 2,
+            ),
+            Expanded(
+              child: Placeholder(),
+              flex: 20,
+            ),
+            Expanded(
+              child: Placeholder(),
+              flex: 2,
+            ),
+          ],
+        ),
       ),
     );
   }
