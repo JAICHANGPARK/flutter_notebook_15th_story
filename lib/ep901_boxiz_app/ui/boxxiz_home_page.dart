8a8f32f8b1e0faa21f2cddfcdfd07ec271ccaf6d
Dreamwalker <aristojeff@gmail.com>
2021-09-04 23:06:41 +0900

    Update boxxiz_home_page.dart

--- lib/ep901_boxiz_app/ui/boxxiz_home_page.dart
+++ lib/ep901_boxiz_app/ui/boxxiz_home_page.dart
@@ -71,7 +71,7 @@ class _BoxizHomePageState extends State<BoxizHomePage> {
                               children: [
                                 Row(
                                   children: [
-                                    
+
                                   ],
                                 )
                               ],
