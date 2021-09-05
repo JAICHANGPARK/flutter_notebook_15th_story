89e8a9d0d73d2234ea900e1b3784893e415c6cbb
Dreamwalker <aristojeff@gmail.com>
2021-09-05 19:32:54 +0900

    Update boxiz_theme.dart

--- lib/ep901_boxiz_app/boxiz_theme.dart
+++ lib/ep901_boxiz_app/boxiz_theme.dart
@@ -4,6 +4,8 @@ class BoxizTheme {
   // static const Color backgroundColor = Color(0xff121d39); //18 29 57
 
   static const Color backgroundColor = Color(0xff000434); //18 29 57
+  //11 16 44
+  static const Color cardColor = Color(0xff0B102C); //18 29 57
 
   static const Color accentColor = Color(0xff04daea); // 4 218 234
 // ThemeData boxizTheme(){
