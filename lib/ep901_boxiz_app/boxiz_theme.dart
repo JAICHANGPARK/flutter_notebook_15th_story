97c747f3555e1462c394ed546e008c1f6ed4927a
Dreamwalker <aristojeff@gmail.com>
2021-09-05 19:31:29 +0900

    Update boxiz_theme.dart

--- lib/ep901_boxiz_app/boxiz_theme.dart
+++ lib/ep901_boxiz_app/boxiz_theme.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class BoxizTheme {
-  static const Color backgroundColor = Color(0xff121d39); //18 29 57
+  // static const Color backgroundColor = Color(0xff121d39); //18 29 57
+
+  static const Color backgroundColor = Color(0xff000434); //18 29 57
+
   static const Color accentColor = Color(0xff04daea); // 4 218 234
 // ThemeData boxizTheme(){
 //   return ThemeData.from(colorScheme: ColorScheme(
