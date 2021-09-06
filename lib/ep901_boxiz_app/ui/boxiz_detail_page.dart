43b60263feb6d37f3cd3c03e4b76ab6aa1303bbe
Dreamwalker <aristojeff@gmail.com>
2021-09-06 18:58:55 +0900

    Update boxiz_detail_page.dart

--- lib/ep901_boxiz_app/ui/boxiz_detail_page.dart
+++ lib/ep901_boxiz_app/ui/boxiz_detail_page.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+
+class BoxizDetailPage extends StatefulWidget {
+  const BoxizDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _BoxizDetailPageState createState() => _BoxizDetailPageState();
+}
+
+class _BoxizDetailPageState extends State<BoxizDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
