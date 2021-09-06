b21f08cd73fd0bbf2b95432af4ba90cea79800bb
Dreamwalker <aristojeff@gmail.com>
2021-09-06 19:03:27 +0900

    Update boxiz_detail_page.dart

--- lib/ep901_boxiz_app/ui/boxiz_detail_page.dart
+++ lib/ep901_boxiz_app/ui/boxiz_detail_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_15th_story/ep901_boxiz_app/model/boxiz.dart';
 
 
 class BoxizDetailPage extends StatefulWidget {
-  const BoxizDetailPage({Key? key}) : super(key: key);
+  final Boxiz boxiz;
+  BoxizDetailPage({Key? key, this.boxiz}) : super(key: key);
 
   @override
   _BoxizDetailPageState createState() => _BoxizDetailPageState();
