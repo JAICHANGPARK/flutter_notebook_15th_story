6e3242c0648364a4c4fcb74f85c1351e84643d52
Dreamwalker <aristojeff@gmail.com>
2021-09-09 19:32:20 +0900

    Update haibooks_home_page.dart

--- lib/ep907_haibooks_app/ui/haibooks_home_page.dart
+++ lib/ep907_haibooks_app/ui/haibooks_home_page.dart
@@ -11,19 +11,47 @@ class _HaibooksHomePageState extends State<HaibooksHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(flex: 4, child: Placeholder()),
-            Expanded(flex: 15, child: Placeholder()),
-          ],
+        backgroundColor: Colors.blueGrey[50],
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(flex: 4, child: Placeholder()),
+              Expanded(flex: 15, child: Placeholder()),
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-
-        ),
-      )
-    );
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 64,
+            padding: ,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.dashboard),
+                      Text("Dashboard"),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: CircleAvatar(
+                    radius: 20,
+                    child: Icon(Icons.add),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.menu), Text("Menu")],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
