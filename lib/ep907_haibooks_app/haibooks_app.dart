c33550e4d988af1c324eaceb84e8fe3f0918ba5c
Dreamwalker <aristojeff@gmail.com>
2021-09-09 19:24:44 +0900

    Update haibooks_app.dart

---
+++ lib/ep907_haibooks_app/haibooks_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HaibooksApp extends StatelessWidget {
+  const HaibooksApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HaibooksHomePage(),
+    );
+  }
+}
