bfa235c2a7cbb49ac259997a3cf803663c4764fa
Dreamwalker <aristojeff@gmail.com>
2021-09-13 18:56:49 +0900

    Update real_estate_app.dart

--- lib/ep911_real_estate_app/real_estate_app.dart
+++ lib/ep911_real_estate_app/real_estate_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'ui/real_estate_home_page.dart';
 
 class RealEstateApp extends StatelessWidget {
   const RealEstateApp({Key? key}) : super(key: key);
@@ -7,7 +8,7 @@ class RealEstateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: RealEstateHomePage(),
     );
   }
 }
