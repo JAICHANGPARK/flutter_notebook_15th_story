3b8d615ed86811ae55e9926b71f30efa5e3530db
Dreamwalker <aristojeff@gmail.com>
2021-09-13 19:09:05 +0900

    Create real_estate.dart

---
+++ lib/ep911_real_estate_app/model/real_estate.dart
@@ -0,0 +1,23 @@
+class RealEstate {
+  String? img;
+  String? tag;
+  String? price;
+  String? vendor;
+  String? title;
+  int? rooms;
+  int? bathroom;
+  int? block;
+  String? type;
+
+  RealEstate({
+    this.img,
+    this.tag,
+    this.price,
+    this.vendor,
+    this.title,
+    this.rooms,
+    this.bathroom,
+    this.block,
+    this.type,
+  });
+}
