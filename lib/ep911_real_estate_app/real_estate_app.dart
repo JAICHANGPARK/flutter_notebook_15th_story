da3c0116d4c91f3e9782eef526125b3104685243
Dreamwalker <aristojeff@gmail.com>
2021-09-13 18:55:22 +0900

    Create real_estate_app.dart

---
+++ lib/ep911_real_estate_app/real_estate_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateApp extends StatelessWidget {
+  const RealEstateApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
