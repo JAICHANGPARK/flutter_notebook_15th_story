9bc62d7457a77a0fd247f4a4b00b49da2da9d52b
Dreamwalker <aristojeff@gmail.com>
2021-09-14 19:04:34 +0900

    Create real_estate_card_widget.dart

---
+++ lib/ep911_real_estate_app/ui/real_estate_card_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateCardWidget extends StatelessWidget {
+  const RealEstateCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
