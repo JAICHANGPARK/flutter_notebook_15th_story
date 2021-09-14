59cd0859fcc18787016ef319d0617379f6ce2d95
Dreamwalker <aristojeff@gmail.com>
2021-09-14 19:10:41 +0900

    Update real_estate_home_page.dart

--- lib/ep911_real_estate_app/ui/real_estate_home_page.dart
+++ lib/ep911_real_estate_app/ui/real_estate_home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_15th_story/ep911_real_estate_app/model/real_estate.dart';
 
+import 'real_estate_card_widget.dart';
+
 class RealEstateHomePage extends StatefulWidget {
   const RealEstateHomePage({Key? key}) : super(key: key);
 
@@ -51,82 +53,9 @@ class _RealEstateHomePageState extends State<RealEstateHomePage> {
               ),
               ...List.generate(
                   realEstateItem.length,
-                  (index) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 200,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                image: DecorationImage(
-                                    image: NetworkImage(realEstateItem[index].img ?? ""), fit: BoxFit.cover)),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 16),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "\$ ${realEstateItem[index].price ?? ""}",
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                                Text(
-                                  realEstateItem[index].vendor ?? "",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Text(
-                            realEstateItem[index].title ?? "",
-                          ),
-                          SizedBox(height: 16),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.bed_outlined,
-                                size: 20,
-                                color: Colors.grey,
-                              ),
-                              SizedBox(width: 4),
-                              Text(
-                                "${realEstateItem[index].rooms ?? 2}",
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                              SizedBox(width: 8),
-                              Icon(
-                                Icons.bathtub_outlined,
-                                size: 20,
-                                color: Colors.grey,
-                              ),
-                              SizedBox(width: 4),
-                              Text(
-                                "${realEstateItem[index].bathroom ?? 2}",
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                              SizedBox(width: 8),
-                              Icon(
-                                Icons.apps,
-                                size: 20,
-                                color: Colors.grey,
-                              ),
-                              SizedBox(width: 4),
-                              Text(
-                                "${realEstateItem[index].block ?? 2}",
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                              Spacer(),
-                              Text(
-                                "${realEstateItem[index].type ?? 2}",
-                                style: TextStyle(color: Colors.grey),
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            height: 16,
-                          ),
-                        ],
-                      ))
+                  (index) => RealEstateCardWidget()
+
+    )
             ],
           ),
         ),
