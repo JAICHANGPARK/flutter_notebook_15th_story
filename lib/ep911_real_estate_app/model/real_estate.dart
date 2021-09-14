2e5f9915bdef867b83e5cb695df0ddb2fa9b9f61
Dreamwalker <aristojeff@gmail.com>
2021-09-14 19:11:26 +0900

    Update real_estate.dart

--- lib/ep911_real_estate_app/model/real_estate.dart
+++ lib/ep911_real_estate_app/model/real_estate.dart
@@ -33,4 +33,14 @@ List<RealEstate> realEstateItem = [
       rooms: 2,
       bathroom: 2,
       block: 2998),
+  RealEstate(
+      tag: "PREMIUM",
+      price: "499,990",
+      img: "https://cdn.pixabay.com/photo/2017/03/22/17/39/kitchen-2165756_960_720.jpg",
+      vendor: "Betterhomes.",
+      title: "Exclusive, serviced, all bills included. City views",
+      type: "Villa",
+      rooms: 2,
+      bathroom: 2,
+      block: 2998),
 ];
