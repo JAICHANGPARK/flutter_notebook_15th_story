83708ef3f60dff67fb9e9565dfbc59bcea641cb7
Dreamwalker <aristojeff@gmail.com>
2021-09-15 18:53:02 +0900

    Update real_estate_detail_page.dart

---
+++ lib/ep911_real_estate_app/ui/real_estate_detail_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_15th_story/ep911_real_estate_app/model/real_estate.dart';
+
+class RealEstateDetailPage extends StatefulWidget {
+  final RealEstate realEstate;
+
+  RealEstateDetailPage({Key? key, required this.realEstate}) : super(key: key);
+
+  @override
+  _RealEstateDetailPageState createState() => _RealEstateDetailPageState();
+}
+
+class _RealEstateDetailPageState extends State<RealEstateDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
