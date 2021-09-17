b837fda84fc249c66ddd497583761893ba453342
Dreamwalker <aristojeff@gmail.com>
2021-09-17 18:47:55 +0900

    Update tma_home_page.dart

--- lib/ep915_task_management_app/ui/tma_home_page.dart
+++ lib/ep915_task_management_app/ui/tma_home_page.dart
@@ -21,14 +21,21 @@ class _TmaHomePageState extends State<TmaHomePage> {
                 child: Container(
                   height: 64,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(16),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        offset: Offset(1, 2)
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.2), offset: Offset(1, 2))]),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.home),
+                          CircleAvatar(
+                            radius: 4,
+                            backgroundColor: Colors.orange,
+                          )
+                        ],
                       )
-                    ]
+                    ],
                   ),
                 ))
           ],
