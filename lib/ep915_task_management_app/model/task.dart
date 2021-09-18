826d9d80201ef967b1603f2f6cebb1e377f73efb
Dreamwalker <aristojeff@gmail.com>
2021-09-18 21:40:46 +0900

    Update task.dart

--- lib/ep915_task_management_app/model/task.dart
+++ lib/ep915_task_management_app/model/task.dart
@@ -1,1 +1,22 @@
-class Task {}
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'task.freezed.dart';
+
+class TaskTag {
+  String? tag;
+  Color? color;
+
+  TaskTag({this.tag, this.color});
+}
+
+@freezed
+class Task with _$Task {
+  factory Task({
+    String? title,
+    String? dueDateTime,
+    String? description,
+    List<TaskTag> taskTags,
+  }) = _Task;
+}
