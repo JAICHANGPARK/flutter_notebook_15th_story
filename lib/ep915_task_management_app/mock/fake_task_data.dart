e32ef967bdd28f520414f9339bcf94c60db40d4a
Dreamwalker <aristojeff@gmail.com>
2021-09-19 17:16:15 +0900

    Update fake_task_data.dart

--- lib/ep915_task_management_app/mock/fake_task_data.dart
+++ lib/ep915_task_management_app/mock/fake_task_data.dart
@@ -1,3 +1,9 @@
 import 'package:flutter_notebook_15th_story/ep915_task_management_app/model/task.dart';
 
-List<Task> taskItems = [];
+List<Task> taskItems = [
+  Task(
+      title: "UX Design for MONS calendar",
+      dueDateTime: "Oct 25, 2021",
+      description: "In publishing and graphic design, Lorem Ipsum is a placeholder",
+      taskTags: [])
+];
