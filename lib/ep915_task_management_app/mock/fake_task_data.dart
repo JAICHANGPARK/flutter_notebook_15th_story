0e9884316f1e7555d1ea4a5162ba95da4e4bec36
Dreamwalker <aristojeff@gmail.com>
2021-09-19 17:21:54 +0900

    Update fake_task_data.dart

--- lib/ep915_task_management_app/mock/fake_task_data.dart
+++ lib/ep915_task_management_app/mock/fake_task_data.dart
@@ -19,4 +19,21 @@ List<Task> taskItems = [
           "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427__340.jpg"
         ],
       )),
+  Task(
+      title: "UX Design for MONS calendar",
+      dueDateTime: "Oct 25, 2021",
+      description: "In publishing and graphic design, Lorem Ipsum is a placeholder",
+      taskTags: [
+        TaskTag(tag: "Normal", color: Colors.blue),
+        TaskTag(tag: "5 Score", color: Colors.deepPurpleAccent),
+      ],
+      taskRelate: TaskRelate(
+        counter: 4,
+        profileItems: [
+          "https://cdn.pixabay.com/photo/2014/04/12/14/59/portrait-322470__340.jpg",
+          "https://cdn.pixabay.com/photo/2020/08/21/08/46/african-5505598__340.jpg",
+          "https://cdn.pixabay.com/photo/2017/04/06/19/34/girl-2209147__340.jpg",
+          "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427__340.jpg"
+        ],
+      )),
 ];
