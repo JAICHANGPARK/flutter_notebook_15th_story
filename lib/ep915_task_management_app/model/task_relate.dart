c9ebd01cdfaf2b4e4f93ec97365ed7c860f27972
Dreamwalker <aristojeff@gmail.com>
2021-09-20 19:08:21 +0900

    Update task_relate.dart

--- lib/ep915_task_management_app/model/task_relate.dart
+++ lib/ep915_task_management_app/model/task_relate.dart
@@ -2,12 +2,11 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
 part 'task_relate.freezed.dart';
-part 'task_relate.g.dart'
 
+part 'task_relate.g.dart';
 
 @freezed
 class TaskRelate with _$TaskRelate {
-
   factory TaskRelate({int? counter, List<String>? profileItems}) = _TaskRelate;
 
   factory TaskRelate.fromJson(Map<String, dynamic> json) => _$TaskRelateFromJson(json);
