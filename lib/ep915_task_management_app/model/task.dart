5e1c7a2ebe3651a9ef8e549adce29a8092138b14
Dreamwalker <aristojeff@gmail.com>
2021-09-20 19:01:19 +0900

    Update task.dart

--- lib/ep915_task_management_app/model/task.dart
+++ lib/ep915_task_management_app/model/task.dart
@@ -4,19 +4,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'task.freezed.dart';
 
-class TaskTag {
-  String? tag;
-  Color? color;
 
-  TaskTag({this.tag, this.color});
-}
 
-class TaskRelate {
-  int? counter;
-  List<String>? profileItems;
 
-  TaskRelate({this.counter, this.profileItems});
-}
 
 @freezed
 class Task with _$Task {
