ee0394da7b113940356777d437daa2aec68adff1
Dreamwalker <aristojeff@gmail.com>
2021-09-21 22:19:55 +0900

    Create tma_detail_page.dart

---
+++ lib/ep915_task_management_app/ui/tma_detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_15th_story/ep915_task_management_app/model/task.dart';
+
+class TmaDetailPage extends StatefulWidget {
+  final Task task;
+
+  TmaDetailPage({Key? key, required this.task}) : super(key: key);
+
+  @override
+  _TmaDetailPageState createState() => _TmaDetailPageState();
+}
+
+class _TmaDetailPageState extends State<TmaDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
