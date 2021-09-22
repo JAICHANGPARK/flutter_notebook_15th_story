85dc17b5976c324e995bfee8fe14742b2f1923d4
Dreamwalker <aristojeff@gmail.com>
2021-09-22 20:47:38 +0900

    Update tma_detail_page.dart

--- lib/ep915_task_management_app/ui/tma_detail_page.dart
+++ lib/ep915_task_management_app/ui/tma_detail_page.dart
@@ -13,6 +13,7 @@ class TmaDetailPage extends StatefulWidget {
 class _TmaDetailPageState extends State<TmaDetailPage> {
   @override
   Widget build(BuildContext context) {
+    var profileLength = widget.task.taskRelate?.profileItems?.length ?? 0;
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -90,6 +91,59 @@ class _TmaDetailPageState extends State<TmaDetailPage> {
                 ],
               ),
             ),
+            SizedBox(
+              height: 48,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: widget.task.taskTags?.length ?? 0,
+                  itemBuilder: (context, index2) {
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                        right: 16,
+                        top: 8,
+                        bottom: 8,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: widget.task.taskTags?[index2]?.color,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        child: Center(
+                          child: Text(
+                            widget.task.taskTags?[index2]?.tag ?? "",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            SizedBox(
+              height: 48,
+              child: Stack(
+                children: [
+                  for (int i = 0; i < profileLength; i++)
+                    Positioned(
+                      left: i * 24,
+                      top: 0,
+                      bottom: 0,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            widget.task.taskRelate?.profileItems?[i] ?? ""),
+                      ),
+                    ),
+                  Positioned(
+                    child: Center(
+                        child: Text("+ ${widget.task.taskRelate?.counter ?? ""}")),
+                    left: 32,
+                    right: 0,
+                    bottom: 0,
+                    top: 0,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
