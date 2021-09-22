26d26a10ab4d51ce30da69847068feefe1e7591a
Dreamwalker <aristojeff@gmail.com>
2021-09-22 20:49:28 +0900

    Update tma_detail_page.dart

--- lib/ep915_task_management_app/ui/tma_detail_page.dart
+++ lib/ep915_task_management_app/ui/tma_detail_page.dart
@@ -17,18 +17,37 @@ class _TmaDetailPageState extends State<TmaDetailPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: InkWell(
-                      onTap: () => Navigator.of(context).pop(),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: InkWell(
+                        onTap: () => Navigator.of(context).pop(),
+                        child: Container(
+                          height: 32,
+                          width: 32,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Icon(Icons.keyboard_arrow_left),
+                          ),
+                        ),
+                      ),
+                    )),
+                    Expanded(child: Center(child: Text("Today task"))),
+                    Expanded(
+                        child: Align(
+                      alignment: Alignment.centerRight,
                       child: Container(
                         height: 32,
                         width: 32,
@@ -37,116 +56,103 @@ class _TmaDetailPageState extends State<TmaDetailPage> {
                           shape: BoxShape.circle,
                         ),
                         child: Center(
-                          child: Icon(Icons.keyboard_arrow_left),
+                          child: Icon(Icons.edit_outlined),
                         ),
                       ),
-                    ),
-                  )),
-                  Expanded(child: Center(child: Text("Today task"))),
-                  Expanded(
-                      child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      height: 32,
-                      width: 32,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: Icon(Icons.edit_outlined),
-                      ),
-                    ),
-                  )),
-                ],
-              ),
-            ),
-            Text(
-              widget.task.title ?? "",
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              children: [
-                CircleAvatar(
-                  radius: 12,
+                    )),
+                  ],
                 ),
-                Text("Created by dreamwalker \"Product Manager\"")
-              ],
-            ),
-            Text("${widget.task.description ?? ""}"
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, "
-                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              child: Row(
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                widget.task.title ?? "",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              Row(
                 children: [
-                  Icon(
-                    Icons.flag,
-                    color: Colors.red,
-                    size: 18,
+                  CircleAvatar(
+                    radius: 12,
                   ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text(
-                    widget.task.dueDateTime ?? "unknown",
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  )
+                  Text("Created by dreamwalker \"Product Manager\"")
                 ],
               ),
-            ),
-            SizedBox(
-              height: 48,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: widget.task.taskTags?.length ?? 0,
-                  itemBuilder: (context, index2) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                        right: 16,
-                        top: 8,
-                        bottom: 8,
-                      ),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: widget.task.taskTags?[index2]?.color,
-                          borderRadius: BorderRadius.circular(16),
+              Text("${widget.task.description ?? ""}"
+                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, "
+                  "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.flag,
+                      color: Colors.red,
+                      size: 18,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      widget.task.dueDateTime ?? "unknown",
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 48,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: widget.task.taskTags?.length ?? 0,
+                    itemBuilder: (context, index2) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                          right: 16,
+                          top: 8,
+                          bottom: 8,
                         ),
-                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        child: Center(
-                          child: Text(
-                            widget.task.taskTags?[index2]?.tag ?? "",
-                            style: TextStyle(color: Colors.white),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: widget.task.taskTags?[index2]?.color,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                          child: Center(
+                            child: Text(
+                              widget.task.taskTags?[index2]?.tag ?? "",
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  }),
-            ),
-            SizedBox(
-              height: 48,
-              child: Stack(
-                children: [
-                  for (int i = 0; i < profileLength; i++)
-                    Positioned(
-                      left: i * 24,
-                      top: 0,
-                      bottom: 0,
-                      child: CircleAvatar(
-                        backgroundImage: NetworkImage(widget.task.taskRelate?.profileItems?[i] ?? ""),
-                      ),
-                    ),
-                  Positioned(
-                    child: Center(child: Text("+ ${widget.task.taskRelate?.counter ?? ""}")),
-                    left: 32,
-                    right: 0,
-                    bottom: 0,
-                    top: 0,
-                  ),
-                ],
+                      );
+                    }),
               ),
-            )
-          ],
+              SizedBox(
+                height: 48,
+                child: Stack(
+                  children: [
+                    for (int i = 0; i < profileLength; i++)
+                      Positioned(
+                        left: i * 24,
+                        top: 0,
+                        bottom: 0,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(widget.task.taskRelate?.profileItems?[i] ?? ""),
+                        ),
+                      ),
+                    Positioned(
+                      child: Center(child: Text("+ ${widget.task.taskRelate?.counter ?? ""}")),
+                      left: 32,
+                      right: 0,
+                      bottom: 0,
+                      top: 0,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
