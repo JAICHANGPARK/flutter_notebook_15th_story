d11ddf3277bfdbb79eb474a9175a6ef38e6c053c
Dreamwalker <aristojeff@gmail.com>
2021-09-23 19:32:36 +0900

    Create nft_profile_page.dart

---
+++ lib/ep921_nft_marketplace_app/ui/nft_profile_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NFTProfilePage extends StatefulWidget {
+  const NFTProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _NFTProfilePageState createState() => _NFTProfilePageState();
+}
+
+class _NFTProfilePageState extends State<NFTProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
