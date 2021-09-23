5eec61a0b656c5f679d433b051660333f9c62a5d
Dreamwalker <aristojeff@gmail.com>
2021-09-23 19:32:52 +0900

    Update nft_marketplace_app.dart

--- lib/ep921_nft_marketplace_app/nft_marketplace_app.dart
+++ lib/ep921_nft_marketplace_app/nft_marketplace_app.dart
@@ -3,13 +3,15 @@
 ///
 import "package:flutter/material.dart";
 
+import 'ui/nft_profile_page.dart';
+
 class NFTMarketPlaceApp extends StatelessWidget {
   const NFTMarketPlaceApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+        home: NFTProfilePage(),
     );
   }
 }
