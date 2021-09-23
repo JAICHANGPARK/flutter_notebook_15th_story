8609986cd0b49883fe6989a1c4790ce1cb1863e0
Dreamwalker <aristojeff@gmail.com>
2021-09-23 19:30:54 +0900

    Update nft_marketplace_app.dart

---
+++ lib/ep921_nft_marketplace_app/nft_marketplace_app.dart
@@ -0,0 +1,13 @@
+/// NFT : Non-Fungible Token
+///
+///
+import "package:flutter/material.dart";
+
+class NFTMarketPlaceApp extends StatelessWidget {
+  const NFTMarketPlaceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
