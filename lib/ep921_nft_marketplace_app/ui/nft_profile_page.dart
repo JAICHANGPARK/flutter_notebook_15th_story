3c361bc64577e2cd0c207265cbf79418aa8be10e
Dreamwalker <aristojeff@gmail.com>
2021-09-23 19:43:17 +0900

    Update nft_profile_page.dart

--- lib/ep921_nft_marketplace_app/ui/nft_profile_page.dart
+++ lib/ep921_nft_marketplace_app/ui/nft_profile_page.dart
@@ -10,34 +10,70 @@ class NFTProfilePage extends StatefulWidget {
 class _NFTProfilePageState extends State<NFTProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.keyboard_arrow_left)),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-                ],
-              ),
-              CircleAvatar(
-                radius: 48,
-                backgroundImage:
-                    NetworkImage("https://cdn.pixabay.com/photo/2016/01/25/06/12/beijing-opera-1160109_960_720.jpg"),
-              ),
-              SizedBox(height: 16,),
-              Text(
-                "Hello World",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text("@unknown"),
-              )
-            ],
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.keyboard_arrow_left)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                  ],
+                ),
+                CircleAvatar(
+                  radius: 48,
+                  backgroundImage:
+                      NetworkImage("https://cdn.pixabay.com/photo/2016/01/25/06/12/beijing-opera-1160109_960_720.jpg"),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Hello World",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("@unknown"),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    minimumSize: Size(0, 0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
+                    " quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.normal, color: Colors.grey),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text("hello@world.com", style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                ),
+                TabBar(
+                  tabs: [
+                    Tab(
+                      text: "Creations",
+                    ),
+                    Tab(
+                      text: "Collections",
+                    ),
+                  ],
+                  indicatorColor: Colors.black,
+                )
+              ],
+            ),
           ),
         ),
       ),
