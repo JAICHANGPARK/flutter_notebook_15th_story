744ef5d94055f0fcb15d52a0ec921813441e64a5
Dreamwalker <aristojeff@gmail.com>
2021-09-24 21:28:25 +0900

    Create nft_defail_page.dart

---
+++ lib/ep921_nft_marketplace_app/ui/nft_defail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NftDetailPage extends StatefulWidget {
+  const NftDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _NftDetailPageState createState() => _NftDetailPageState();
+}
+
+class _NftDetailPageState extends State<NftDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
