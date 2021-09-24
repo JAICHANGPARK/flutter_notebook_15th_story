8fb8c80dc6022a27a7bbe30e4feaff9ccabe8ac3
Dreamwalker <aristojeff@gmail.com>
2021-09-24 21:36:30 +0900

    Update nft_detail_page.dart

--- lib/ep921_nft_marketplace_app/ui/nft_detail_page.dart
+++ lib/ep921_nft_marketplace_app/ui/nft_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class NftDetailPage extends StatefulWidget {
   const NftDetailPage({Key? key}) : super(key: key);
 
@@ -12,11 +11,44 @@ class _NftDetailPageState extends State<NftDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(child: Placeholder()),
-          Expanded(child: Placeholder()),
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(24),
+                      image: DecorationImage(
+                          image:
+                              NetworkImage("https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg"),
+                          fit: BoxFit.cover)),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          top: 16,
+
+                          child:Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(onPressed: () {}, icon: Icon(Icons.keyboard_arrow_left)),
+                              IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                            ],
+                          ),)
+                    ],
+                  ),
+                ),
+                flex: 10,
+              ),
+              Expanded(
+                child: Placeholder(),
+                flex: 11,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
