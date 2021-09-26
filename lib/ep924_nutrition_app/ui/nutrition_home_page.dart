70ac38c4e0adbcaed272f29ca1081a88d4e26380
Dreamwalker <aristojeff@gmail.com>
2021-09-26 20:41:38 +0900

    Update nutrition_home_page.dart

---
+++ lib/ep924_nutrition_app/ui/nutrition_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NutritionHomePage extends StatefulWidget {
+  const NutritionHomePage({Key? key}) : super(key: key);
+
+  @override
+  _NutritionHomePageState createState() => _NutritionHomePageState();
+}
+
+class _NutritionHomePageState extends State<NutritionHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
