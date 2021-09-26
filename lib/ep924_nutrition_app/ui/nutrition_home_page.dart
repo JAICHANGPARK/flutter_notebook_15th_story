7ed1d3991df2547e71e5680eda278f1ad650ddf8
Dreamwalker <aristojeff@gmail.com>
2021-09-26 20:48:33 +0900

    Update nutrition_home_page.dart

--- lib/ep924_nutrition_app/ui/nutrition_home_page.dart
+++ lib/ep924_nutrition_app/ui/nutrition_home_page.dart
@@ -12,41 +12,88 @@ class _NutritionHomePageState extends State<NutritionHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Expanded(child: Placeholder(), flex: 15,),
-              Expanded(child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children:[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              Expanded(
+                child: Placeholder(),
+                flex: 15,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-
-                    Icon(Icons.home, size: 28,),
-                    SizedBox(height: 4,),
-                    CircleAvatar(radius: 4,)
-                  ],),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-
-                      Icon(Icons.search, size: 28,),
-                      SizedBox(height: 4,),
-                      // CircleAvatar(radius: 4,)
-                    ],),
-                  Container(
-                    child: IconButton(
-                      icon: Icon(Icons.qr_code_scanner), onPressed: () {  },
-                    ),
-                    decoration: BoxDecoration(
-                    color: Colors.deepPurpleAccent[100],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.home,
+                            size: 28,
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          CircleAvatar(
+                            radius: 4,
+                          )
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.search,
+                            size: 28,
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          // CircleAvatar(radius: 4,)
+                        ],
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(Icons.qr_code_scanner),
+                          onPressed: () {},
+                        ),
+                        decoration:
+                            BoxDecoration(color: Colors.deepPurpleAccent[100], borderRadius: BorderRadius.circular(8)),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.bar_chart,
+                            size: 28,
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          // CircleAvatar(radius: 4,)
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.person,
+                            size: 28,
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          // CircleAvatar(radius: 4,)
+                        ],
+                      ),
+                    ],
                   ),
-                  )
-
-              ],), flex: 2,)
+                ),
+                flex: 2,
+              )
             ],
           ),
         ),
