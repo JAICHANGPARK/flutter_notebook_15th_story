61e728a040ebe3b4b67bc5fc614cfebd43e1a722
Dreamwalker <aristojeff@gmail.com>
2021-09-28 19:10:29 +0900

    Update nutrition_home_page.dart

--- lib/ep924_nutrition_app/ui/nutrition_home_page.dart
+++ lib/ep924_nutrition_app/ui/nutrition_home_page.dart
@@ -106,6 +106,7 @@ class _NutritionHomePageState extends State<NutritionHomePage> {
                                       )
                                     ],
                                   )),
+                              SizedBox(width: 24,),
                               Expanded(
                                   flex: 4,
                                   child: Column(
