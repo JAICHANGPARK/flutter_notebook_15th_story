b504762a6d68525dc9fd504f5217358e28e9dc9f
Dreamwalker <aristojeff@gmail.com>
2021-09-28 19:05:39 +0900

    Update nutrition_home_page.dart

--- lib/ep924_nutrition_app/ui/nutrition_home_page.dart
+++ lib/ep924_nutrition_app/ui/nutrition_home_page.dart
@@ -9,6 +9,12 @@ class NutritionHomePage extends StatefulWidget {
 }
 
 class _NutritionHomePageState extends State<NutritionHomePage> {
+  /// 101 95 177 65 5f b1
+  /// 110 105 182  6e 69 b6
+
+  Color purpleColor = Color(0xff655fb1);
+  Color purpleLightColor = Color(0xff6e69b6);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
