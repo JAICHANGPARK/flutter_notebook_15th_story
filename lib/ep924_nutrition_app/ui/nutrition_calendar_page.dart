43d7efdcdce29339b34c693048d4f520551d0411
Dreamwalker <aristojeff@gmail.com>
2021-09-29 18:47:00 +0900

    Create nutrition_calendar_page.dart

---
+++ lib/ep924_nutrition_app/ui/nutrition_calendar_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class NutritionCalendarPage extends StatefulWidget {
+  const NutritionCalendarPage({Key? key}) : super(key: key);
+
+  @override
+  _NutritionCalendarPageState createState() => _NutritionCalendarPageState();
+}
+
+class _NutritionCalendarPageState extends State<NutritionCalendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
