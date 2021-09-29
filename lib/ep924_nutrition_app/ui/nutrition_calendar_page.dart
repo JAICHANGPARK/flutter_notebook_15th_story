061ce291e2cdb6dc1cd2c583adfc69abbea6bba8
Dreamwalker <aristojeff@gmail.com>
2021-09-29 18:53:53 +0900

    Update nutrition_calendar_page.dart

--- lib/ep924_nutrition_app/ui/nutrition_calendar_page.dart
+++ lib/ep924_nutrition_app/ui/nutrition_calendar_page.dart
@@ -363,6 +363,144 @@ class _NutritionCalendarPageState extends State<NutritionCalendarPage> {
                         ),
                       ),
                     ),
+                  SizedBox(height: 16,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              CircularPercentIndicator(
+                                radius: 26,
+                                percent: 0.6,
+                                lineWidth: 4,
+                                progressColor: Colors.teal,
+                                backgroundColor: Colors.teal[100]!,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Expanded(
+                                child: VerticalDivider(
+                                  color: Colors.grey,
+                                  width: 3,
+                                  thickness: 1.5,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Dinner",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                          fontSize: 16),
+                                    ),
+                                    Text(
+                                      "407 kcal",
+                                      style: TextStyle(color: Colors.grey),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 16,
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 48,
+                                      width: 48,
+                                      decoration: BoxDecoration(
+                                          color: Colors.blue[50],
+                                          borderRadius:
+                                          BorderRadius.circular(8)),
+                                      child: Center(
+                                        child: Text(
+                                          "☕",
+                                          style: TextStyle(fontSize: 24),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Espresso coffee",
+                                            style: TextStyle(
+                                                fontWeight:
+                                                FontWeight.bold),
+                                          ),
+                                          SizedBox(
+                                            height: 4,
+                                          ),
+                                          Text("30 ml")
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 16,
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 48,
+                                      width: 48,
+                                      decoration: BoxDecoration(
+                                          color: Colors.brown[50],
+                                          borderRadius:
+                                          BorderRadius.circular(8)),
+                                      child: Center(
+                                        child: Text(
+                                          "🥐",
+                                          style: TextStyle(fontSize: 24),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Croissant",
+                                            style: TextStyle(
+                                                fontWeight:
+                                                FontWeight.bold),
+                                          ),
+                                          SizedBox(
+                                            height: 4,
+                                          ),
+                                          Text("100 g")
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
                
               ),
