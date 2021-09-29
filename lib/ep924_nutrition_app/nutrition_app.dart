214554a892b4ecf866d444992b3a4124a98ca938
Dreamwalker <aristojeff@gmail.com>
2021-09-29 18:48:55 +0900

    Update nutrition_app.dart

--- lib/ep924_nutrition_app/nutrition_app.dart
+++ lib/ep924_nutrition_app/nutrition_app.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'ui/nutrition_home_page.dart';
 
+/// 101 95 177 65 5f b1
+/// 110 105 182  6e 69 b6
+Color purpleColor = Color(0xff655fb1);
+Color purpleLightColor = Color(0xff6e69b6);
 
 class NutritionApp extends StatelessWidget {
   const NutritionApp({Key? key}) : super(key: key);
