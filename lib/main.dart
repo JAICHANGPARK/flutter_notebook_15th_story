e60b198fffdfd8198745c728e27130f2fd5a342a
Dreamwalker <aristojeff@gmail.com>
2021-09-30 19:36:13 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'ep924_nutrition_app/nutrition_app.dart';
+import 'ep928_elearning_app/e_learning_app.dart';
 
-main() => runApp(NutritionApp());
+main() => runApp(ELearningApp());
