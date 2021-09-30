697027ff470bb58c47e07766a6e86582fc5baace
Dreamwalker <aristojeff@gmail.com>
2021-09-30 19:34:27 +0900

    Create e_learning_app.dart

---
+++ lib/ep928_elearning_app/e_learning_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ELearingApp extends StatelessWidget {
+  const ELearingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
