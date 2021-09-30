f63c22af0c0a5be0dbb6263d14954734164cf2c7
Dreamwalker <aristojeff@gmail.com>
2021-09-30 19:35:25 +0900

    Update e_learning_home_page.dart

---
+++ lib/ep928_elearning_app/ui/e_learning_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ELearningHomePage extends StatefulWidget {
+  const ELearningHomePage({Key? key}) : super(key: key);
+
+  @override
+  _ELearningHomePageState createState() => _ELearningHomePageState();
+}
+
+class _ELearningHomePageState extends State<ELearningHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
