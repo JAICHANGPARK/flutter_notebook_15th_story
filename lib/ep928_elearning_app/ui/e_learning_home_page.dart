70b897689b3666ff2e348f7162b8ae69b4680453
Dreamwalker <aristojeff@gmail.com>
2021-09-30 19:41:35 +0900

    Update e_learning_home_page.dart

--- lib/ep928_elearning_app/ui/e_learning_home_page.dart
+++ lib/ep928_elearning_app/ui/e_learning_home_page.dart
@@ -12,21 +12,46 @@ class _ELearningHomePageState extends State<ELearningHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Placeholder(),
-              flex: 1,
-            ),
-            Expanded(
-              child: SingleChildScrollView(),
-              flex: 10,
-            ),
-            Expanded(
-              child: Placeholder(),
-              flex: 2,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: PhysicalModel(
+                        color: Colors.black.withOpacity(0.2),
+                        elevation: 2,
+                        child: Container(
+                          height: 38,
+                          width: 38,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle
+                          ),
+                          padding: EdgeInsets.all(4),
+                          child: Center(child: Icon(Icons.arrow_back_ios)),
+                        ),
+                      ),
+                    )),
+                    Expanded(child: Center(child: Text("Course Detail"))),
+                    Expanded(child: Placeholder()),
+                  ],
+                ),
+                flex: 1,
+              ),
+              Expanded(
+                child: SingleChildScrollView(),
+                flex: 10,
+              ),
+              Expanded(
+                child: Placeholder(),
+                flex: 2,
+              ),
+            ],
+          ),
         ),
       ),
     );
