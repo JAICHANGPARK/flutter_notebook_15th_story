329b841bdc338d8fefe236cfc5e6cb3f7341b2d2
Dreamwalker <aristojeff@gmail.com>
2021-10-03 19:00:39 +0900

    Update find_job_main_page.dart

--- lib/ep931_fine_job_app/ui/find_job_main_page.dart
+++ lib/ep931_fine_job_app/ui/find_job_main_page.dart
@@ -11,15 +11,25 @@ class _FindJobMainPageState extends State<FindJobMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            child: Container(),
-            left: 0,
-            right: 0,
-            bottom: 0,
-          ),
-        ],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                height: 64,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    )),
+              ),
+              left: 0,
+              right: 0,
+              bottom: 0,
+            ),
+          ],
+        ),
       ),
     );
   }
