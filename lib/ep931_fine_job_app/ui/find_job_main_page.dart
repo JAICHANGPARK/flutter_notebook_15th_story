b1a2ab1d63a8b024d3171658bedfd70c92247df5
Dreamwalker <aristojeff@gmail.com>
2021-10-03 19:00:10 +0900

    Update find_job_main_page.dart

--- lib/ep931_fine_job_app/ui/find_job_main_page.dart
+++ lib/ep931_fine_job_app/ui/find_job_main_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class FindJobMainPage extends StatefulWidget {
   const FindJobMainPage({Key? key}) : super(key: key);
 
@@ -12,6 +10,17 @@ class FindJobMainPage extends StatefulWidget {
 class _FindJobMainPageState extends State<FindJobMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            child: Container(),
+            left: 0,
+            right: 0,
+            bottom: 0,
+          ),
+        ],
+      ),
+    );
   }
 }
