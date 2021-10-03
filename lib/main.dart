5a9c034e548f077c4bab1e4e3cee60f1583eb642
Dreamwalker <aristojeff@gmail.com>
2021-10-03 18:59:11 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'ep924_nutrition_app/nutrition_app.dart';
-import 'ep928_elearning_app/e_learning_app.dart';
+import 'ep931_fine_job_app/fine_job_app.dart';
 
-main() => runApp(ELearningApp());
+main() => runApp(FindJobApp());
