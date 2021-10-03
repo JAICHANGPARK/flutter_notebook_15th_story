637b3b1318bd4d2a71e3336359010087807b4b81
Dreamwalker <aristojeff@gmail.com>
2021-10-03 18:57:58 +0900

    Update fine_job_app.dart

---
+++ lib/ep931_fine_job_app/fine_job_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FindJobApp extends StatelessWidget {
+  const FindJobApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
