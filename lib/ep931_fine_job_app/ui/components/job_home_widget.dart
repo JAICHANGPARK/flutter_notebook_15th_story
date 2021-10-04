59a55c83ec7c02c57c9ca11cc122515fdba9e8c2
Dreamwalker <aristojeff@gmail.com>
2021-10-04 18:43:41 +0900

    Create job_home_widget.dart

---
+++ lib/ep931_fine_job_app/ui/components/job_home_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JobHomeWidget extends StatefulWidget {
+  const JobHomeWidget({Key? key}) : super(key: key);
+
+  @override
+  _JobHomeWidgetState createState() => _JobHomeWidgetState();
+}
+
+class _JobHomeWidgetState extends State<JobHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
