2fb4c012ab4a736756ec33ac958892e39484382f
Dreamwalker <aristojeff@gmail.com>
2021-10-04 18:45:09 +0900

    Create job_favorite_widget.dart

---
+++ lib/ep931_fine_job_app/ui/components/job_favorite_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class JobFavoriteWidget extends StatefulWidget {
+  const JobFavoriteWidget({Key? key}) : super(key: key);
+
+  @override
+  _JobFavoriteWidgetState createState() => _JobFavoriteWidgetState();
+}
+
+class _JobFavoriteWidgetState extends State<JobFavoriteWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
