44d6910f16211e59797fa576c61a2e07bcf4daa9
Dreamwalker <aristojeff@gmail.com>
2021-10-06 19:54:36 +0900

    Create find_job_detail_page.dart

---
+++ lib/ep931_fine_job_app/ui/find_job_detail_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class FindJobDetailPage extends StatefulWidget {
+  const FindJobDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _FindJobDetailPageState createState() => _FindJobDetailPageState();
+}
+
+class _FindJobDetailPageState extends State<FindJobDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
