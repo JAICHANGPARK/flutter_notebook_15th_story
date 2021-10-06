3fe7b112aaf14a6bcbc5aefe13d6db6ffabff564
Dreamwalker <aristojeff@gmail.com>
2021-10-06 20:12:00 +0900

    Update find_job_detail_page.dart

--- lib/ep931_fine_job_app/ui/find_job_detail_page.dart
+++ lib/ep931_fine_job_app/ui/find_job_detail_page.dart
@@ -154,6 +154,11 @@ class _FindJobDetailPageState extends State<FindJobDetailPage> {
                       SizedBox(
                         height: 4,
                       ),
+                      Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey
+                      ),)
                     ],
                   ),
                 )),
