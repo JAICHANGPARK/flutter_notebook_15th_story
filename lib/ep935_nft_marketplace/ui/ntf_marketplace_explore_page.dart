07f3ec051fcc1dff63c712f91c5ccc6b9e093234
Dreamwalker <aristojeff@gmail.com>
2021-10-07 20:05:04 +0900

    Update ntf_marketplace_explore_page.dart

---
+++ lib/ep935_nft_marketplace/ui/ntf_marketplace_explore_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class NtfMarketplaceExplorePage extends StatefulWidget {
+  const NtfMarketplaceExplorePage({Key? key}) : super(key: key);
+
+  @override
+  _NtfMarketplaceExplorePageState createState() => _NtfMarketplaceExplorePageState();
+}
+
+class _NtfMarketplaceExplorePageState extends State<NtfMarketplaceExplorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
