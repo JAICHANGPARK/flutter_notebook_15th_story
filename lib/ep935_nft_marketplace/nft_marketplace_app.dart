60e1a9282c47ebefe37237d809af68305169b2b2
Dreamwalker <aristojeff@gmail.com>
2021-10-07 19:58:50 +0900

    Update nft_marketplace_app.dart

--- lib/ep935_nft_marketplace/nft_marketplace_app.dart
+++ lib/ep935_nft_marketplace/nft_marketplace_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_15th_story/ep935_nft_marketplace/ui/ntf_marketplace_home_page.dart';
 
 
 class NftMarketPlaceApp extends StatelessWidget {
@@ -7,6 +8,7 @@ class NftMarketPlaceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: NftMarketplaceHomePage(),
 
     );
   }
