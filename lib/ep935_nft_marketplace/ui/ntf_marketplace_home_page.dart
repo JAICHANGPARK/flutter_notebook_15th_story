0f9c4e0c13f53ef24565583d8edaa26251c550c9
Dreamwalker <aristojeff@gmail.com>
2021-10-07 19:57:30 +0900

    Create ntf_marketplace_home_page.dart

---
+++ lib/ep935_nft_marketplace/ui/ntf_marketplace_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NftMarketplaceHomePage extends StatefulWidget {
+  const NftMarketplaceHomePage({Key? key}) : super(key: key);
+
+  @override
+  _NftMarketplaceHomePageState createState() => _NftMarketplaceHomePageState();
+}
+
+class _NftMarketplaceHomePageState extends State<NftMarketplaceHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
