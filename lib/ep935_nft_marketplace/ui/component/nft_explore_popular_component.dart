cb1c2657745f2b5effa6d94fa4d12385237c7b0c
Dreamwalker <aristojeff@gmail.com>
2021-10-09 20:33:22 +0900

    Update nft_explore_popular_component.dart

--- lib/ep935_nft_marketplace/ui/component/nft_explore_popular_component.dart
+++ lib/ep935_nft_marketplace/ui/component/nft_explore_popular_component.dart
@@ -11,41 +11,46 @@ class _NftExplorePopularComponentState extends State<NftExplorePopularComponent>
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 360,
-            child: Card(
-              child: Stack(
-                children: [
-                  Positioned(
-                      left: 16,
-                      top: 16,
-                      right: 16,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 240,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: NetworkImage(
-                                        "https://cdn.pixabay.com/photo/2021/04/29/05/04/"
-                                        "desert-6215516_960_720.jpg"),
-                                    fit: BoxFit.cover)),
-                          ),
-                          Text(
-                            "Sky on the ring",
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text("5 Edition art")
-                        ],
-                      ))
-                ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 360,
+              child: Card(
+                child: Stack(
+                  children: [
+                    Positioned(
+                        left: 16,
+                        top: 16,
+                        right: 16,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 200,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          "https://cdn.pixabay.com/photo/2021/04/29/05/04/"
+                                          "desert-6215516_960_720.jpg"),
+                                      fit: BoxFit.cover)),
+                            ),
+                            SizedBox(height: 8,),
+                            Text(
+                              "Sky on the ring",
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            SizedBox(height: 4,),
+                            Text("5 Edition art")
+                          ],
+                        ))
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
