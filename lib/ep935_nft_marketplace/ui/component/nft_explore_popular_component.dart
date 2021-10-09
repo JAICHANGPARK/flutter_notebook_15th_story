61a993f0ac9cb54b5790ec0335c97dceb153d3e8
Dreamwalker <aristojeff@gmail.com>
2021-10-09 20:32:36 +0900

    Update nft_explore_popular_component.dart

--- lib/ep935_nft_marketplace/ui/component/nft_explore_popular_component.dart
+++ lib/ep935_nft_marketplace/ui/component/nft_explore_popular_component.dart
@@ -23,15 +23,22 @@ class _NftExplorePopularComponentState extends State<NftExplorePopularComponent>
                       top: 16,
                       right: 16,
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
+                            height: 240,
                             decoration: BoxDecoration(
                                 image: DecorationImage(
                                     image: NetworkImage(
                                         "https://cdn.pixabay.com/photo/2021/04/29/05/04/"
-                                            "desert-6215516_960_720.jpg"),
+                                        "desert-6215516_960_720.jpg"),
                                     fit: BoxFit.cover)),
-                          )
+                          ),
+                          Text(
+                            "Sky on the ring",
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text("5 Edition art")
                         ],
                       ))
                 ],
