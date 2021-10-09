ba6e84462230a4a269727bcc0023409067d82821
Dreamwalker <aristojeff@gmail.com>
2021-10-09 20:27:21 +0900

    Update files

--- lib/ep935_nft_marketplace/nft_marketplace_app.dart
+++ lib/ep935_nft_marketplace/nft_marketplace_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_15th_story/ep935_nft_marketplace/ui/ntf_marketplace_home_page.dart';
+import 'package:flutter_notebook_15th_story/ep935_nft_marketplace/ui/pages/ntf_marketplace_home_page.dart';
 
 
 class NftMarketPlaceApp extends StatelessWidget {
