e1abe02723171f21c6cd4537d2e230a299352da5
Dreamwalker <aristojeff@gmail.com>
2021-10-09 20:28:20 +0900

    Create nft_explore_popular_component.dart

---
+++ lib/ep935_nft_marketplace/ui/component/nft_explore_popular_component.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NftExplorePopularComponent extends StatefulWidget {
+  const NftExplorePopularComponent({Key? key}) : super(key: key);
+
+  @override
+  _NftExplorePopularComponentState createState() => _NftExplorePopularComponentState();
+}
+
+class _NftExplorePopularComponentState extends State<NftExplorePopularComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
