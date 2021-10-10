d45fbd3bf9922a8fc4d515819614479608607626
Dreamwalker <aristojeff@gmail.com>
2021-10-10 18:09:23 +0900

    Create educational_home_page.dart

---
+++ lib/ep938_educational_app/src/ui/educational_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class EducationalHomePage extends StatefulWidget {
+  const EducationalHomePage({Key? key}) : super(key: key);
+
+  @override
+  _EducationalHomePageState createState() => _EducationalHomePageState();
+}
+
+class _EducationalHomePageState extends State<EducationalHomePage>
+with TickerProviderStateMixin{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
