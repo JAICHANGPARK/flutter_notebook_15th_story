89c87cf4dfd13f37a0a6ad9c9523185a2cc292d0
Dreamwalker <aristojeff@gmail.com>
2021-10-10 18:08:44 +0900

    Create educational_app.dart

---
+++ lib/ep938_educational_app/educational_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class EducationalApp extends StatelessWidget {
+  const EducationalApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
