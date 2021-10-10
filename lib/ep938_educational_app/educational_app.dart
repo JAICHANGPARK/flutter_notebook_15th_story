b6d1110649a7a42594ab91e4097a06634b276fd0
Dreamwalker <aristojeff@gmail.com>
2021-10-10 18:09:57 +0900

    Update educational_app.dart

--- lib/ep938_educational_app/educational_app.dart
+++ lib/ep938_educational_app/educational_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/ui/educational_home_page.dart';
+
 
 class EducationalApp extends StatelessWidget {
   const EducationalApp({Key? key}) : super(key: key);
@@ -7,7 +9,9 @@ class EducationalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      routes: {
+        "/":(context)=>EducationalHomePage()
+      },
     );
   }
 }
