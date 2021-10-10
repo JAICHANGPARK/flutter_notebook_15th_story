d8aff1c948a47eedce8829b652a3dbb5cb3590ee
Dreamwalker <aristojeff@gmail.com>
2021-10-10 18:11:06 +0900

    Update educational_home_page.dart

--- lib/ep938_educational_app/src/ui/educational_home_page.dart
+++ lib/ep938_educational_app/src/ui/educational_home_page.dart
@@ -13,7 +13,14 @@ with TickerProviderStateMixin{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: Placeholder()),
+            Expanded(child: Placeholder())
+          ],
+        ),
+      ),
     );
   }
 }
