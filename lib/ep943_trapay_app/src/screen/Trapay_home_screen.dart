7dd2cc5f80b96cec5e898c74a4ff46d6a4ed8cd2
Dreamwalker <aristojeff@gmail.com>
2021-10-15 18:51:07 +0900

    Create Trapay_home_screen.dart

---
+++ lib/ep943_trapay_app/src/screen/Trapay_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrapayHomeScreen extends StatefulWidget {
+  const TrapayHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _TrapayHomeScreenState createState() => _TrapayHomeScreenState();
+}
+
+class _TrapayHomeScreenState extends State<TrapayHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
