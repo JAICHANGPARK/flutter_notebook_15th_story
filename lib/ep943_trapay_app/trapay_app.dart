722aa7a8a3e3af4cf3d41c848a10d9a2424c678d
Dreamwalker <aristojeff@gmail.com>
2021-10-16 18:18:57 +0900

    Update trapay_app.dart

--- lib/ep943_trapay_app/trapay_app.dart
+++ lib/ep943_trapay_app/trapay_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_15th_story/ep943_trapay_app/src/screen/trapay_home_screen.dart';
+import "package:flutter_localizations/flutter_localizations.dart";
 
 
 class TrapayApp extends StatelessWidget {
@@ -9,6 +10,15 @@ class TrapayApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       restorationScopeId: "trapayApp",
+      localizationsDelegates: [
+        AppLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''), // English, no country code
+      ],
       home: TrapayHomeScreen(),
     );
   }
