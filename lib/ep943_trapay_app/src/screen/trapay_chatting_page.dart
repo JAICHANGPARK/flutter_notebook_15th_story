77f18ad473968472829bd8bc488bf0e5b6d8fcb7
Dreamwalker <aristojeff@gmail.com>
2021-10-17 18:58:25 +0900

    Create trapay_chatting_page.dart

---
+++ lib/ep943_trapay_app/src/screen/trapay_chatting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrapayChattingPage extends StatefulWidget {
+  const TrapayChattingPage({Key? key}) : super(key: key);
+
+  @override
+  _TrapayChattingPageState createState() => _TrapayChattingPageState();
+}
+
+class _TrapayChattingPageState extends State<TrapayChattingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
