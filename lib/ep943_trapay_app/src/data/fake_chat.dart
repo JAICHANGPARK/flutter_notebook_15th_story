f15b4eba3eac391fcf98a5a0470de4ffbb806cb1
Dreamwalker <aristojeff@gmail.com>
2021-10-18 19:34:37 +0900

    Update fake_chat.dart

--- lib/ep943_trapay_app/src/data/fake_chat.dart
+++ lib/ep943_trapay_app/src/data/fake_chat.dart
@@ -1,12 +1,12 @@
-
-
 import 'package:flutter_notebook_15th_story/ep943_trapay_app/src/model/trapay_chat.dart';
 
-List<TrapayChat> trapayChat = [
+List<TrapayChat> trapayChatItems = [
   TrapayChat(
-    avatarImg: "https://cdn.pixabay.com/photo/2021/09/15/11/34/woman-6626615_960_720.jpg",
-    name: "Dreamwalker",
-    message: ""
-  )
-
+      avatarImg: "https://cdn.pixabay.com/photo/2021/09/15/11/34/woman-6626615_960_720.jpg",
+      name: "Dreamwalker",
+      message: "How about development Flutter?",
+      isRead: true,
+      msgTime: "12:30",
+      newMsgCount: 0),
+  TrapayChat(name: "Dreamwalker", message: "How about development Flutter?", isRead: true, msgTime: "12:30", newMsgCount: 2)
 ];
