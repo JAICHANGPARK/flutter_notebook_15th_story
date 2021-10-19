2be47711286e5918dd21468a2cdaef225bd3f23d
Dreamwalker <aristojeff@gmail.com>
2021-10-19 19:15:37 +0900

    Update groceries_app.dart

--- lib/ep947_groceries_app/groceries_app.dart
+++ lib/ep947_groceries_app/groceries_app.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class GroceriesApp extends StatelessWidget {
+  const GroceriesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
