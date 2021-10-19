fa41dd3c2a257e88564f5ebbedf37f069793bbaa
Dreamwalker <aristojeff@gmail.com>
2021-10-19 19:17:30 +0900

    Update groceries_main_page.dart

--- lib/ep947_groceries_app/src/groceries_main_page.dart
+++ lib/ep947_groceries_app/src/groceries_main_page.dart
@@ -1,2 +1,21 @@
 import 'package:flutter/material.dart';
 
+class GroceriesMainPage extends StatefulWidget {
+  const GroceriesMainPage({Key? key}) : super(key: key);
+
+  @override
+  _GroceriesMainPageState createState() => _GroceriesMainPageState();
+}
+
+class _GroceriesMainPageState extends State<GroceriesMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
