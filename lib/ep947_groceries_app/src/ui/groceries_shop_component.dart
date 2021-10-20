e9a994a4ded71a46473d1bf9a349f10caa371a06
Dreamwalker <aristojeff@gmail.com>
2021-10-20 19:19:42 +0900

    Update groceries_shop_component.dart

---
+++ lib/ep947_groceries_app/src/ui/groceries_shop_component.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class GroceriesShopComponent extends StatefulWidget {
+  const GroceriesShopComponent({Key? key}) : super(key: key);
+
+  @override
+  _GroceriesShopComponentState createState() => _GroceriesShopComponentState();
+}
+
+class _GroceriesShopComponentState extends State<GroceriesShopComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
