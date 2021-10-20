af032853294f4b6b3f216b30c45df08e97a222db
Dreamwalker <aristojeff@gmail.com>
2021-10-20 19:31:34 +0900

    Update groceries_shop_component.dart

--- lib/ep947_groceries_app/src/ui/groceries_shop_component.dart
+++ lib/ep947_groceries_app/src/ui/groceries_shop_component.dart
@@ -12,60 +12,72 @@ class _GroceriesShopComponentState extends State<GroceriesShopComponent> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Column(
-        children: [
-          PhysicalModel(
-            color: Colors.black.withOpacity(0.1),
-            elevation: 2,
-            child: Container(
-              color: Colors.white,
-              child: TabBar(labelColor: Colors.black, unselectedLabelColor: Colors.grey, indicatorColor: Colors.green, tabs: [
-                Tab(
-                  text: "Shops",
-                ),
-                Tab(
-                  text: "Lists",
-                )
-              ]),
-            ),
-          ),
-          Expanded(
-            child: TabBarView(
-              children: [
-                SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Container(
-                          height: 48,
-                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(36),
-                          ),
-                          child: TextField(
-                            controller: _textEditingController,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Search everything",
-                              icon: Icon(Icons.search),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
+    return Container(
+      color: Colors.grey[50],
+      child: DefaultTabController(
+
+        length: 2,
+        child: Column(
+          children: [
+            PhysicalModel(
+              color: Colors.black.withOpacity(0.1),
+              elevation: 2,
+              child: Container(
+                color: Colors.white,
+                child: TabBar(labelColor: Colors.black, unselectedLabelColor: Colors.grey, indicatorColor: Colors.green, tabs: [
+                  Tab(
+                    text: "Shops",
                   ),
-                ),
-                ListView.builder(itemBuilder: (context, index) {
-                  return Container();
-                })
-              ],
+                  Tab(
+                    text: "Lists",
+                  )
+                ]),
+              ),
             ),
-          )
-        ],
+            Expanded(
+              child: TabBarView(
+                children: [
+                  SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                  height: 48,
+                                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[200],
+                                    borderRadius: BorderRadius.circular(36),
+                                  ),
+                                  child: TextField(
+                                    controller: _textEditingController,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Search everything",
+                                      icon: Icon(Icons.search),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+
+                      ],
+                    ),
+                  ),
+                  ListView.builder(itemBuilder: (context, index) {
+                    return Container();
+                  })
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
