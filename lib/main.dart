ef5405fe398a014286b18edf1ecd3943b96cf9be
Dreamwalker <aristojeff@gmail.com>
2021-10-22 23:24:22 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'ep947_groceries_app/groceries_app.dart';
 
-main() => runApp(GroceriesApp());
+main() => runApp(ProviderScope(child: GroceriesApp()));
