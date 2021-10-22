0c314017468e27255080adce77b4cacc90d0cdcf
Dreamwalker <aristojeff@gmail.com>
2021-10-22 23:20:18 +0900

    Update bottom_index_controller.dart

--- lib/ep947_groceries_app/src/controller/bottom_index_controller.dart
+++ lib/ep947_groceries_app/src/controller/bottom_index_controller.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final bottomIndexProvider = StateNotifierProvider((ref) => 0);
+final bottomIndexProvider = StateNotifierProvider<BottomIndexController, int>((ref) => BottomIndexController(0));
 
 class BottomIndexController extends StateNotifier<int> {
   BottomIndexController(int state) : super(state);
+
   setIndex(int i) => state = i;
 }
